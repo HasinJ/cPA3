58ace726c4365f99e52375fc28370f539364846a
HasinJ <hasinchoudhuryj@gmail.com>
2020-10-30 11:02:19 -0400

    almsot there

--- fifth/fifth.c
+++ fifth/fifth.c
@@ -5,58 +5,66 @@
 
 int length=0;
 
-void printBinary(signed long int decimal, signed long int bits){
-  for (signed long int i = bits-1; i!=-1; i--) {
-    signed long int mask = 1lu<<i;
-    mask = mask & decimal;
-    mask = mask>>(i);
-    printf("%ld", mask);
+void printBinary(signed long int decimal, signed long int bits, int count){
+  if(count!=0){
+    for (signed long int i = count-1; bits!=0; i--) {
+      if(i<0) {
+        bits--;
+        printf("0");
+        continue;}
+      signed long int mask = 1lu<<i;
+      mask = mask & decimal;
+      mask = mask>>(i);
+      printf("%ld", mask);
+      bits--;
+    }
+
   }
-  printf("\n");
+
+  if(count==0){
+    for (signed long int i = bits-1; i!=-1; i--) {
+      signed long int mask = 1lu<<i;
+      mask = mask & decimal;
+      mask = mask>>(i);
+      printf("%ld", mask);
+    }
+
+  }
 }
 
-void printFrac(signed long int decimal, signed long int bits){
-  if(length<bits) length = bits;
-  for (signed long int i = length-1; i!=-1; i--) {
-    if(bits==-1) break;
-    signed long int mask = 1lu<<i;
-    mask = mask & decimal;
-    mask = mask>>(i);
-    printf("%ld", mask);
-    bits--;
-  }
-  printf("\n");
-}
 
-signed long int without1(signed long int decimal, signed long int bits){
-  signed long int j;
-  for (j = 32; j!=-1; j--) {
-    signed long int mask = 1lu<<j;
-    mask = mask & decimal;
-    mask = mask>>(j);
-    if(mask == 1) break;
-  }
-  j--;
-  signed long int frac = 0b0;
-  for (signed long int i = j; i!=-1; i--) {
-    signed long int mask = 1lu<<i;
-    mask = mask & decimal;
-    mask = mask>>(i);
-    if(mask==1) frac = (frac<<1)|(1<<0); //adds 1
-    if(mask==0) frac = (frac<<1); //adds 0
-    length++;
-  }
-  //printf("%d\n", length);
-  return frac;
-}
+signed long int roundMantissa(signed long int mantissa, signed long int bits, int count){
+  unsigned long int last = 0b0;
+  unsigned long int plus1 = 0b0;
+  unsigned long int sticky = 1;
+  if(count>bits){
+    for (count-=1; bits!=0; count--) {
+      //printf("countfirst: %d\n", count);
 
-signed long int roundMantissa(signed long int mantissa,signed long int frac_bits){
-  if(length==frac_bits) return mantissa;
-  signed long int g = (mantissa & 0b0100)>>2;
-  signed long int s = (mantissa & 0b0010)>>1;
-  signed long int r = mantissa & 0b0001;
+      last = ((1lu<<(count)) & mantissa) >> (count);
+      plus1 = ((1lu<<(count-1)) & mantissa) >> (count-1);
+      bits--;
+    }
+    unsigned long int temp = 0;
+    //printf("count: %d\n", count);
+    for (; count!=0; count--) {
+      if((((1lu<<(count)) & mantissa) >> (count)) == 1) temp = (temp<<1)|(1<<0); //adds 1
+      if((((1lu<<(count)) & mantissa) >> (count)) == 0) temp = (temp<<1); //adds 0
+    }
+    if(temp==0) sticky = 0;
 
-  if(g==1 && (r==1 || s==1)) mantissa += 1;
+  }
+
+  //printf("last: %lu\n", last);
+  //printf("plus1: %lu\n", plus1);
+  //printf("sticky: %lu\n", sticky);
+  if((last==1 && plus1==1) || (plus1==1 && sticky==1)){
+    mantissa+=1;
+  }
+
+  if(count==0){
+    return mantissa;
+  }
 
   return mantissa;
 }
@@ -87,69 +95,49 @@ int main(int argc, char const *argv[argc+1]) {
     signed long int bias = (1lu << (exp_bits-1))-1;
     signed long int integer_part = (int) number;
     double decimal_part = number - integer_part;
-    //printf("%f\n", decimal_part);
 
     while(number>2){
       number = number/2.0;
       e++;
     }
+    if(number>1) number-=1;
+    decimal_part = number;
+    signed long int exponent = e + bias;
 
     //printf("e:%d\n", e);
 
     //rightside
-    signed long int frac = integer_part;
-    signed long int count = 0;
+    signed long int mantissa = 0;
+    int count = 0;
      while(decimal_part!=decimal_part-decimal_part){ //needs changing around
        if(count==33) break;
        //printf("%f\n", decimal_part);
        decimal_part = decimal_part*2.0;
        if(decimal_part >= 1) {
          decimal_part-=1.0;
-         frac = (frac<<1)|(1<<0); //adds 1
+         mantissa = (mantissa<<1)|(1<<0); //adds 1
          count++;
          continue;
        }
        count++;
-       frac = (frac<<1); //adds 0
+       mantissa = (mantissa<<1); //adds 0
      }
+     //printf("count: %d\n", count);
+     //printf("mantissa: ");
+     //printBinary(mantissa,count,0);
 
-     //where it finds 1 on the rightside
-     if (integer_part==0) {
-       signed long int i = frac_bits;
-       signed long int temp = 0;
-       while(temp!=1){
-         i--;
-         if(i==frac_bits) break;
-         temp = 1lu<<i & frac;
-         temp=temp>>i;
-       }
-       e = i - frac_bits;
-       //printf("new e:%d\n", e);
-     }
-     //printBinary(frac,32);
-     //printf("\n");
+     //printf("sign: ");
+     printBinary(sign,bits-(exp_bits+frac_bits),0);
 
-     printf("sign: ");
-     printBinary(sign,bits-(exp_bits+frac_bits));
+     //printf("exponent: ");
+     printBinary(exponent,exp_bits,0);
 
-     signed long int exponent = e + bias;
-     printf("exponent: ");
-     printBinary(exponent,exp_bits);
+     mantissa = roundMantissa(mantissa,frac_bits,count);
+     //printf("mantissa rounded: ");
+     //printBinary(mantissa,count,0);
 
-     printf("frac old: ");
-     printBinary(frac,20);
-
-     frac = without1(frac,frac_bits);
-     printf("frac without 1: ");
-     printBinary(frac,32);
-
-
-     frac = roundMantissa(frac,frac_bits);
-     printf("frac rounded: ");
-     printBinary(frac,20);
-
-     printf("frac new:");
-     printFrac(frac,frac_bits);
+     //printf("mantissa actual: ");
+     printBinary(mantissa,frac_bits,count);
 
      printf("\n");
 
